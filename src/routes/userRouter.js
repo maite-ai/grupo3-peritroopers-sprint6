f3bb909b0f172c5055e1f6e6eb0acdbe3eb6bccf
matiasbenavide <matiasbenavide@gmail.com>
2021-06-23 21:54:47 -0300

    avances con el CRUD

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,14 +1,13 @@
 const express = require('express');
+
 const router = express.Router();
-const controller = require('../controller/usersController');
-const uploadFile=require('../middlewares/multerMiddleware')
-const validations=require('../middlewares/userValidationsMiddleware')
-const authMiddleware=require('../middlewares/authMiddleware')
-const guestMiddleware=require('../middlewares/guestMiddleware')
-router.get('/register', guestMiddleware, controller.register);
-router.post('/register', uploadFile.single('avatar'), validations, controller.processRegister);
-router.get('/login', guestMiddleware, controller.login);
-router.post('/login', controller.loginProcess);
-router.get('/profile/', authMiddleware, controller.profile);
-router.get('/logout/', controller.logout)
+
+const userController = require('../controller/usersController')
+
+router.get('/:id', userController.detail)
+router.get('/create', userController.add)
+router.post('/create', userController.create)
+router.get('/edit/:id', userController.edit)
+router.put('/edit/:id', userController.update)
+
 module.exports = router;
