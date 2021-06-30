d34935bed2f34f2d376a40de84fa4ade1199b9f8
peirios <mariafernandarios89@gmail.com>
2021-06-30 18:35:27 -0300

    Pei - CorrecciÃon de algunitos errorcitos

--- src/database/models/Shipping.js
+++ src/database/models/Shipping.js
@@ -4,7 +4,7 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, dataTypes) => {
   class Shipping extends Model {
-    static associate = function(models) {
+    static associate(models) {
       // hasMany
       Shipping.hasMany(models.Purchase, {
           foreignKey: 'shippingId',
